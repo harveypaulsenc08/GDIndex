a940b54f7bf1472ae6b870a47eb2dc3eb4b7a1af
harveypaulsenc08 <87550655+harveypaulsenc08@users.noreply.github.com>
2021-07-17 08:05:17 +0530

    2.0.21mod

--- worker/worker-beta.js
+++ worker/worker-beta.js
@@ -58,15 +58,15 @@ const authConfig = {
 ░░░╚═╝░░░╚═╝░░╚═╝╚══════╝░╚═════╝░╚══════╝╚═════╝░*/
 
 const uiConfig = {
-    "theme": "slate", // switch between themes, default set to vapor, select from https://www.npmjs.com/package/@googledrive/index
+    "theme": "darkly", // switch between themes, default set to vapor, select from https://www.npmjs.com/package/@googledrive/index
     "version": "2.0.21mod", // don't touch this one. get latest code using generator at https://bdi-generator.hashhackers.com
     // If you're using Image then set to true, If you want text then set it to false
-    "logo_image": true, // true if you're using image link in next option.
+    "logo_image": false, // true if you're using image link in next option.
     "logo_height": "", // only if logo_image is true
     "logo_width": "100px", // only if logo_image is true
     "favicon": "https://cdn.jsdelivr.net/gh/harveypaulsenc08/GDIndex@2.0.21mod/images/favicon.ico",
     // if logo is true then link otherwise just text for name
-    "logo_link_name": "https://cdn.jsdelivr.net/gh/harveypaulsenc08/GDIndex@2.0.21mod/images/bhadoo-cloud-logo-white.svg",
+    "logo_link_name": "EXID Video Archive",
     "fixed_header": true, // If you want the footer to be flexible or fixed.
     "header_padding": "60", // Value 60 for fixed header, Value 20 for flexible header. Required to be changed accordingly in some themes.
     "nav_link_1": "Home", // change navigation link name
@@ -84,17 +84,17 @@ const uiConfig = {
     "path_nav_alert_class": "alert alert-primary", // https://getbootstrap.com/docs/4.0/components/alerts/#examples
     "file_view_alert_class": "alert alert-danger", // https://getbootstrap.com/docs/4.0/components/alerts/#examples
     "file_count_alert_class": "alert alert-secondary", // https://getbootstrap.com/docs/4.0/components/alerts/#examples
-    "contact_link": "https://telegram.dog/Telegram", // Link to Contact Button on Menu
-    "copyright_year": "2050", // year of copyright, can be anything like 2015 - 2020 or just 2020
-    "company_name": "Bhadoo Cloud", // Name next to copyright
-    "company_link": "https://telegram.dog/Telegram", // link of copyright name
-    "credit": true, // Set this to true to give us credit
-    "display_size": true, // Set this to false to hide display file size
+    "contact_link": "https://twitter.com/exidvidarchive", // Link to Contact Button on Menu
+    "copyright_year": "2021", // year of copyright, can be anything like 2015 - 2020 or just 2020
+    "company_name": "EXID Video Archive", // Name next to copyright
+    "company_link": "https://twitter.com/exidvidarchive", // link of copyright name
+    "credit": false, // Set this to true to give us credit
+    "display_size": false, // Set this to false to hide display file size
     "display_time": false, // Set this to false to hide display modified time for folder and files
-    "display_download": true, // Set this to false to hide download icon for folder and files on main index
+    "display_download": false, // Set this to false to hide download icon for folder and files on main index
     "disable_player": false, // Set this to true to hide audio and video players
     "custom_srt_lang": "", // Subtitle Language Code for Custom .vtt language.
-    "disable_video_download": false, // Remove Download, Copy Button on Videos
+    "disable_video_download": true, // Remove Download, Copy Button on Videos
     "second_domain_for_dl": false, // If you want to display other URL for Downloading to protect your main domain.
     "downloaddomain": domain_for_dl, // Ignore this and set domains at top of this page after service accounts.
     "poster": "https://cdn.jsdelivr.net/gh/harveypaulsenc08/GDIndex@2.0.21mod/images/poster.jpg", // Video poster URL or see Readme to how to load from Drive
@@ -103,7 +103,7 @@ const uiConfig = {
     "render_head_md": true, // Render Head.md
     "render_readme_md": true, // Render Readme.md
     "display_drive_link": false, // This will add a Link Button to Google Drive of that particular file.
-    "plyr_io_version": "3.6.4", // Change plyr.io version in future when needed.
+    "plyr_io_version": "3.6.8", // Change plyr.io version in future when needed.
     "plyr_io_video_resolution": "16:9", // For reference, visit: https://github.com/sampotts/plyr#options
     "unauthorized_owner_link": "https://telegram.dog/Telegram", // Unauthorized Error Page Link to Owner
     "unauthorized_owner_email": "abuse@telegram.org", // Unauthorized Error Page Owner Email
@@ -137,7 +137,6 @@ function html(current_drive_order = 0, model = {}) {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no"/>
   <title>${authConfig.siteName}</title>
-  <script async src="https://arc.io/widget.min.js#${uiConfig.arc_code}"></script>
   <meta name="robots" content="noindex" />
   <link rel="icon" href="${uiConfig.favicon}">
   <script>
